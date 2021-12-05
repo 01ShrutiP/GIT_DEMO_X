674062e2be7d3b11abf17427ebce6203f8fcaa44
01ShrutiP <parkar.shruti41@gmail.com>
2021-12-05 12:17:04 +0530

    DemoJava file get added in  master repo

---
+++ JavaPractice/src/javaPack/DemoJava.java
@@ -0,0 +1,13 @@
+package javaPack;
+
+public class DemoJava {
+
+	public static void main(String[] args) 
+	{
+	// TODO Auto-generated method stub
+		
+		System.out.println("Hi I am shruti...");
+
+	}
+
+}
